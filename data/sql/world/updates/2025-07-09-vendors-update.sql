178465a4a81262e8af9c2c20e227df6a7c581aff
Abracadaniel222 <178446201+abracadaniel222@users.noreply.github.com>
2025-07-09 04:15:48 +0000

    Add Dalaran Hearthstone to some innkeepers

---
+++ data/sql/world/updates/2025-07-09-vendors-update.sql
@@ -0,0 +1,6 @@
+-- Innkeeper Allison in Stormwind
+INSERT INTO npc_vendor (entry, item) VALUES (6740, 666);
+-- Innkeeper Gryshka in Orgrimmar
+INSERT INTO npc_vendor (entry, item) VALUES (6929, 666);
+-- Amisi Azuregaze in Dalaran
+INSERT INTO npc_vendor (entry, item) VALUES (28687, 666);
