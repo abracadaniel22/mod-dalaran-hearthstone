a0f48c86ebb564d3bd450e1658003647ccc0b9c4
abracadaniel22 <7702641+abracadaniel22@users.noreply.github.com>
2025-07-09 00:44:54 -0400

    Make sql script idempotent

--- data/sql/world/updates/2025-07-09-vendors-update.sql
+++ data/sql/world/updates/2025-07-09-vendors-update.sql
@@ -1,6 +1,20 @@
 -- Innkeeper Allison in Stormwind
-INSERT INTO npc_vendor (entry, item) VALUES (6740, 666);
+INSERT INTO npc_vendor (entry, item)
+SELECT 6740, 666
+WHERE NOT EXISTS (
+  SELECT 1 FROM npc_vendor WHERE entry = 6740 AND item = 666
+);
+
 -- Innkeeper Gryshka in Orgrimmar
-INSERT INTO npc_vendor (entry, item) VALUES (6929, 666);
+INSERT INTO npc_vendor (entry, item)
+SELECT 6929, 666
+WHERE NOT EXISTS (
+  SELECT 1 FROM npc_vendor WHERE entry = 6929 AND item = 666
+);
+
 -- Amisi Azuregaze in Dalaran
-INSERT INTO npc_vendor (entry, item) VALUES (28687, 666);
+INSERT INTO npc_vendor (entry, item)
+SELECT 28687, 666
+WHERE NOT EXISTS (
+  SELECT 1 FROM npc_vendor WHERE entry = 28687 AND item = 666
+);
